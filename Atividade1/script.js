693f1aea7cc5581c2920f34e8ef0fe1bc2b14366
leonardogoandete <leonardo.goandete@hotmail.com>
2021-10-19 19:46:30 -0300

    teste

---
+++ Atividade1/script.js
@@ -0,0 +1,19 @@
+function hello(){
+    let ourName = "Leonardo";
+    document.write(ourName);
+    console.log("Executou a função Hello com sucesso!"); // exibe no console uma informação se não houver erros.
+    teste();
+}
+
+function teste(){
+    let age = 24;
+    document.write(age);
+    console.log("Executou a função Teste com sucesso!"); // exibe no console uma informação se não houver erros.
+    palavra();
+}
+
+function palavra(){
+    let x = "Hello World";
+    document.write();
+    console.log(x);
+}
